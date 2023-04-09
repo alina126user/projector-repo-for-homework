56bee875b98e9adcc54fa608cd174cc1bfc50ac8
alina126user <yablochnikova18@gmail.com>
2023-04-09 13:43:32 +0300

    Homework1

---
+++ required-homework1.js
@@ -0,0 +1,32 @@
+//Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+let initials = [];
+for (let i = 0; i < userNames.length; i++ ) {
+    let stringName = userNames[i].split(" ");
+    let firstLetter = stringName.map(function(word) {
+        return word.charAt(0);      
+    });
+    let result = [firstLetter[0], firstLetter[1], firstLetter[2]].join(".");
+    initials.push(result);
+}
+initials = initials.sort();
+
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+
+//Задача на розворот числа
+
+const currentMaxValue = 4589;
+let stringMaxValue = String(currentMaxValue).split('').reverse().join('');
+let reverseMaxValue = Number(stringMaxValue);
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+//Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:
+
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray = resultsArray.flat(Infinity).reduce((accumulator, nextItem, position, 
+    array) => {
+        return accumulator * nextItem
+    });
+    
+console.log(productOfArray); // 24
